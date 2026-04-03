a665067fc892887e2c57f171a38315f39135bc9b
Karan Aswal <161558653+Karan7088@users.noreply.github.com>
2026-04-03 12:28:18 +0530

    Create schema and data.sql

---
+++ Advanced (FAANG  Product Companies)/Median Salary/schema and data.sql
@@ -0,0 +1,27 @@
+CREATE TABLE employees (
+    employee_id INT auto_increment PRIMARY KEY,
+    employee_name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+INSERT INTO employees (employee_name, department, salary) VALUES
+('Amit', 'IT', 30000),
+('Rohit', 'IT', 45000),
+('Neha', 'HR', 50000),
+('Simran', 'HR', 50000),
+('Karan', 'Finance', 70000),
+('Ankit', 'Finance', 80000),
+('Priya', 'IT', 45000),
+('Vikas', 'IT', 60000),
+('Pooja', 'HR', 55000),
+('Rahul', 'Finance', 90000),
+('Sneha', 'IT', 30000),
+('Arjun', 'Finance', 75000),
+('Meena', 'HR', 52000),
+('Suresh', 'IT', 47000),
+('Kavita', 'Finance', 88000),
+('Deepak', 'HR', 51000),
+('Ramesh', 'IT', 61000),
+('Nisha', 'Finance', 72000),
+('Manoj', 'HR', 53000),
+('Tina', 'IT', 48000);
